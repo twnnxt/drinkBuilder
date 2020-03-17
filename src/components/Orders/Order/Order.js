8c91b6a1a505f25dcd7b712351b94ffdd10498f4
ross <twnnxt@gmail.com>
2020-03-17 12:16:10 +0800

    2nd

--- src/components/Orders/Order/Order.js
+++ src/components/Orders/Order/Order.js
@@ -1,38 +1,3 @@
-// import React from 'react';
-
-// import classes from './Order.module.css';
-
-// const Order = props => {
-//     console.log(props)
-//     return (
-//         <div className={classes.BuildControl} onClick={props.removed}>
-//             <div
-//                 onClick={(e) => props.clicked(e)}
-//                 onInput={(e) => props.changed(e, "name")}
-//                 onBlur={(e) => props.changed(e, "name")}
-//                 placeholder="Please add drink"
-//                 contentEditable="true">{props.name}</div>
-//             <div
-//                 onClick={(e) => props.clicked(e)}
-//                 onInput={(e) => props.changed(e, "price")}
-//                 onBlur={(e) => props.changed(e, "price")}
-//                 placeholder="Please add price"
-//                 contentEditable="true">{props.price}</div>
-//             <label>Notes: </label>
-//             <select
-//                 onClick={(e) => props.clicked(e)}
-//                 onChange={(e) => props.changed(e, "notes")} >
-//                 {props.options.map((opt, i) => (
-//                     <option key={i} value={opt.value}>{opt.displayValue}</option>
-//                 ))}
-//             </select>
-//         </div>
-//     )
-// }
-
-// export default Order;
-
-
 import React from 'react';
 
 import classes from './Order.module.css';
